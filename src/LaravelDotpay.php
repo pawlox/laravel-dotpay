6a0be28ec0db9744d8eefc57e9b010fa497f911b
Alzo02 <alzo02@icloud.com>
2016-08-10 14:41:53 +0200

    init composer

---
+++ src/LaravelDotpay.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Alzo\LaravelDotpay;
+
+use Illuminate\Contracts\Foundation\Application;
+
+class LaravelDotpay
+{
+    public function createForm($data)
+    {
+
+    }
+}
---
+++ src/ServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Alzo\Dotpay;
+
+use Alzo\LaravelDotpay\LaravelDotpay;
+use Illuminate\Routing\Router;
+use Illuminate\Session\SessionManager;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $configPath = __DIR__ . '/../config/laravel-dotpay.php';
+        $this->mergeConfigFrom($configPath, 'laravel-dotpay');
+
+        $this->app->singleton('laravel-dotpay', function ($app) {
+            return new LaravelDotpay();
+        });
+
+        $this->app->alias('laravel-dotpay', 'Alzo\LaravelDotpay\LaravelDotpay');
+    }
+}
